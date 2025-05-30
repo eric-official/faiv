e447ef08cf6fff469594e2c986279ef4b29fe40c
eric-official <eric.naeser@gmx.de>
2025-05-30 22:11:25 +0200

    new stuff

---
+++ src/vlayer/KrakenProver.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {Proof} from "vlayer-0.1.0/Proof.sol";
+import {Prover} from "vlayer-0.1.0/Prover.sol";
+import {Web, WebProof, WebProofLib, WebLib} from "vlayer-0.1.0/WebProof.sol";
+
+contract KrakenProver is Prover {
+    using WebProofLib for WebProof;
+    using WebLib for Web;
+
+    //string public constant DATA_URL = "https://api.kraken.com/0/public/Ticker?pair=ETHUSD";
+    string public constant DATA_URL = "https://api.stripe.com/v1/disputes";
+
+    function main(WebProof calldata webProof) public view returns (Proof memory, bool) {
+        Web memory web = webProof.verify(DATA_URL);
+
+        //string memory avgPrice = web.jsonGetString("result.XETHZUSD.p[0]");
+        bool zeroDisputes = web.jsonGetBool("length(data) == `0`");
+
+        return (proof(), zeroDisputes);
+    }
+}
